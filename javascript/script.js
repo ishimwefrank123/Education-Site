5cdbf95e9b7a9625486a421dce33e78db2e98881
ishimwefrank123 <140093486+ishimwefrank123@users.noreply.github.com>
2024-11-10 21:17:09 +0200

    Creation and add the features of the course features include images and title

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
 //change navbar styles on scroll
 window.addEventListener('scroll', () => {
-  document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+  document.querySelector('nav').classList.toggle('window-scroll', window.scrollY >    0);
 });
