ce34ae592eb2ac37fdb499f55eac586b88f7f51c
ishimwefrank123 <140093486+ishimwefrank123@users.noreply.github.com>
2024-11-11 21:54:42 +0200

    show and hide the frequently asked questions answer and change the icon

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,19 @@
 window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle('window-scroll', window.scrollY >    0);
 });
+
+//show hiden faq answer
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+  faq.addEventListener('click', () =>{
+    faq.classList.toggle('open');
+
+    //change the icon
+    const icon = faq.querySelector('.faq__icon i');
+    if(icon.className === 'bx bx-plus'){
+      icon.className = 'bx bx-minus'
+    }else{
+      icon.className = 'bx bx-plus'
+    }
+  })
+})
